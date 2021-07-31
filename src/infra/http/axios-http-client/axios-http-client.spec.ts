b78035aae4f5acee1d9ee5bc6da3540787730733
Bruno Souza <souza.brunoha@gmail.com>
2021-07-30 23:36:29 -0300

    test: ensure AxiosHttpClient calls axios with correct body

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -17,12 +17,12 @@ const mockPostRequest = (): HttpPostParams<any> => ({
 })
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct URL and verb', async () => {
+  test('Should call axios with correct URL, verb and body', async () => {
     const sut = makeSut()
     const request = mockPostRequest()
 
-    await sut.post({ url: request.url })
+    await sut.post(request)
 
-    expect(mockedAxios.post).toHaveBeenCalledWith(request.url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
 })
