6fbaf26a0d4e9da2b549f93c8fa9078ef14f3f2c
Bruno Souza <souza.brunoha@gmail.com>
2021-08-02 21:11:01 -0300

    chore: add testing library

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -8,8 +8,16 @@ const Login: React.FC = () => {
       <LoginHeader/>
       <form className={Styles.form}>
         <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu e-mail"/>
-        <Input type="password" name="password" placeholder="Digite sua senha"/>
+        <Input
+          type="email"
+          name="email"
+          placeholder="Digite seu e-mail"
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+        />
         <button className={Styles.submit} type="submit">Entrar</button>
         <span className={Styles.link}>Criar conta</span>
         <FormStatus/>
