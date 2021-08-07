304f4555afee54c84e039f3bd2f0728d6b18e446
Bruno Souza <souza.brunoha@gmail.com>
2021-08-06 22:37:53 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validation-composite/validation-composite.spec.ts
+++ src/validation/validation-composite/validation-composite.spec.ts
@@ -1,6 +1,25 @@
 import { FieldValidationSpy } from '@/validation/test'
 import { ValidationComposite } from './validate-composite'
 
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidationsSpy: FieldValidationSpy[]
+}
+
+const makeSut = (): SutTypes => {
+  const fieldValidationsSpy = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field')
+  ]
+
+  const sut = new ValidationComposite(fieldValidationsSpy)
+
+  return {
+    sut,
+    fieldValidationsSpy
+  }
+}
+
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
