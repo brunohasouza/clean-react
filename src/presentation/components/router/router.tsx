8b556b33023781b26ff3882559a688837e3d9b31
Bruno Souza <souza.brunoha@gmail.com>
2021-08-07 10:59:40 -0300

    refactor: move factories to their own files

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import { Login } from '@/presentation/pages'
-
 type Props = {
   makeLogin: React.FC
 }
