4ff76be44757c97401f9bd990941761dbb09f829
Bruno Souza <souza.brunoha@gmail.com>
2021-08-27 23:20:37 -0300

    feat: add SignupValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,10 @@
+import { ValidationBuilder as Builder, ValidationComposite } from '@/validation/validators'
+
+export const makeSignupValidation = (): ValidationComposite => {
+  return ValidationComposite.build([
+    ...Builder.field('name').required().minLength(5).build(),
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('password').required().minLength(5).build(),
+    ...Builder.field('passwordConfirmation').required().sameAs('password').build(),
+  ])
+}
