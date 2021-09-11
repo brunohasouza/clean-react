e11663934edc76fabd1a2ba56a7191cf3411d6cd
Bruno Souza <souza.brunoha@gmail.com>
2021-09-11 11:17:54 -0300

    refactor: change test to use faker

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -92,8 +92,8 @@ describe('Login', () => {
       }
     })
 
-    cy.getByTestId('email').focus().type('mango@gmail.com')    
-    cy.getByTestId('password').focus().type('12345')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
     cy.getByTestId('submit').click()
     cy.getByTestId('spinner').should('not.exist')
     cy.getByTestId('main-error').should('not.exist')
